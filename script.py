0339f97cc7a06f4faa74e66695ec72a58c867e48
Titash RC <titash01@live.in>
2022-01-02 23:53:05 +0100

    need to add more content.

---
+++ script.py
@@ -0,0 +1,9 @@
+"""https://www.youtube.com/watch?v=qUeud6DvOWI&t=180s"""
+
+# use f strings whenever possible
+
+# open file or database connections with WITH command
+
+# avoid use FINALLY in try catch block, in python most resources have their own context manager
+
+# 
